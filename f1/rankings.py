c9b2808db1f4423344eacaa6c908e0c865d07453
Jeronimo Lopes <lopes.jeronimo@gmail.com>
2020-01-01 21:35:22 -0300

    new ranking: score_by_runnerup_titles_at_that_time small refactors

--- f1/rankings.py
+++ f1/rankings.py
@@ -1,24 +1,34 @@
 import csv
-from collections import OrderedDict
+from collections import OrderedDict, defaultdict
 
 
 class ChampionshipRankings:
 
     def __init__(self):
         with open('datasets/champions_by_season.csv') as csvfile:
-            reader = csv.DictReader(csvfile)
-            self.dataset = list(reader)
-
-        self.list_of_champions = [r['champion'] for r in self.dataset]
+            self.dataset = list(csv.DictReader(csvfile))
 
     def score_by_runnerup_titles(self) -> OrderedDict:
         """
         Season champion score must be equals to the number of titles of the runner up
         """
-        resultset = {}
+        resultset = defaultdict(int)
+        list_of_champions = [r['champion'] for r in self.dataset]
         for season in self.dataset:
             champion = season['champion']
-            resultset.setdefault(champion, 0)
-            resultset[champion] += self.list_of_champions.count(season['runner'])
+            resultset[champion] += list_of_champions.count(season['runner'])
+
+        return OrderedDict(sorted(resultset.items(), key=lambda x: x[1], reverse=True))
+
+    def score_by_runnerup_titles_at_that_time(self) -> OrderedDict:
+        """
+         Season champion score must be equals to the number of titles the runner up holds on that year
+        """
+        titles_by_driver = defaultdict(int)
+        resultset = defaultdict(int)
+        for season in self.dataset:
+            champion = season['champion']
+            titles_by_driver[champion] += 1
+            resultset[champion] += titles_by_driver.get(season['runner'], 0)
 
         return OrderedDict(sorted(resultset.items(), key=lambda x: x[1], reverse=True))
--- tests/test_rankings.py
+++ tests/test_rankings.py
@@ -13,6 +13,12 @@ class ChampionshipRankingsTestCase(unittest.TestCase):
         self.assertEqual(scores['Kimi Raikkonen'], 6)
         self.assertEqual(scores['Jacques Villeneuve'], 0)
 
+    def test_score_by_runnerup_titles_at_that_time(self):
+        scores = self.ranking.score_by_runnerup_titles_at_that_time()
+        self.assertEqual(scores['Ayrton Senna'], 5)
+        self.assertEqual(scores['Michael Schumacher'], 2)
+        self.assertEqual(scores['Kimi Raikkonen'], 0)
+
 
 if __name__ == '__main__':
     unittest.main()
